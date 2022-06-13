5d5859d056ec8aac8e3655ffdfd917dded64c11e
joaorodriguesjr <mail@joaorodriguesjr.com>
2022-06-13 15:26:57 -0300

    Change Next JS defaults

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/cardapio',
+        permanent: true,
+      },
+    ]
+  },
+  i18n: {
+    locales: ['pt-BR','en-US'],
+    defaultLocale: 'pt-BR',
+  }
 }
 
 module.exports = nextConfig
---
+++ pages/cardapio.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+
+export default function DeliveryMenu() {
+  return <>
+    <Head>
+      <title>Cardápio - Família Delivery</title>
+    </Head>
+    ...
+  </>
+}

05b2795d7fa2ca5599654fe272dcd59837fa821e
joaorodriguesjr <mail@joaorodriguesjr.com>
2022-06-13 17:09:20 -0300

    Add service information

--- pages/cardapio.jsx
+++ pages/cardapio.jsx
@@ -5,19 +5,28 @@ export default function DeliveryMenu() {
   return <>
     <Head>
       <title>Cardápio - Família Delivery</title>
+      <meta name="description" content="O seu almoço do fim de semana entregue na sua casa" />
     </Head>
     <main className={module.container}>
       <header className={module.header}>
-        <img src="/logo.svg" alt="logo Família Delivery" />
+        <img src="/logo.svg" alt="logo Família Delivery" width={125} height={84}/>
         <div className={module.separator}></div>
         <div className={module.header_content}>
           <h1>Cardápio</h1>
-          <a href="https://wa.me/5531991047243" target='_blank' rel="noreferrer">
-            <img src="/whatsapp.svg"/>
+          <a className={module.action} href="https://wa.me/5531991047243" target='_blank' rel="noreferrer">
+            <img src="/whatsapp.svg" width={16} height={16} alt="ícone do whatsapp"/>
             <span>Agendar Pedido</span>
           </a>
         </div>
       </header>
+      <aside className={module.service}>
+        <h2>Atendimento</h2>
+        <p>Serviço aos sábados e domingos das 11:00 às 14:00. Pedidos de segunda à sexta.</p>
+        <a className={module.action} href="https://wa.me/5531991047243" target='_blank' rel="noreferrer">
+          <img src="/whatsapp.svg" width={16} height={16} alt="ícone do whatsapp"/>
+          <span>Agendar Pedido</span>
+        </a>
+      </aside>
     </main>
   </>
 }

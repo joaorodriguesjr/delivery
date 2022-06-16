542c011c4670d93bf7829681d0d5f86d5d1bec47
joaorodriguesjr <mail@joaorodriguesjr.com>
2022-06-16 09:46:08 -0300

    Change classes naming

--- components/cardapio/item.jsx
+++ components/cardapio/item.jsx
@@ -1,23 +1,23 @@
 import Image from 'next/image'
-import module from './item.module.css'
+import styles from './item.module.css'
 
 export default function Item({ item }) {
   return (
-    <div className={module.item}>
-      <div className={module.recipe}>
+    <div className={styles.container}>
+      <div className={styles.recipe}>
         <h2>{item.recipe.name}</h2>
-        <div className={module.picture} onClick={() => {}}>
+        <div className={styles.picture} onClick={() => {}}>
           <Image priority={true} src={item.recipe.image} alt={item.recipe.alt} width={192} height={85}/>
         </div>
         <p>{item.recipe.ingredients}</p>
       </div>
 
-      <div className={module.portions}>
+      <div className={styles.servings}>
         { item.servings.map((serving, index) => (
-          <div key={index} className={module.portion}>
+          <div key={index} className={styles.serving}>
             <h3>{serving.type}</h3>
             <p>{serving.serve}</p>
-            <p className={module.price}>{serving.price}</p>
+            <p className={styles.price}>{serving.price}</p>
           </div>
         )) }
       </div>
